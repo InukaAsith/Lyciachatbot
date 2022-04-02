e87a7bda06133ba4e795a26b921d1938cc70c3ab
mragasupport <99508889+AzeMusic@users.noreply.github.com>
2022-04-02 11:35:15 +0400

    Update start.py

--- lycia/Lycia/start.py
+++ lycia/Lycia/start.py
@@ -3,13 +3,13 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 LYCIA_START = """
-Hello, I am Lycia [リュキア], An Intelligent ChatBot. If You Are Feeling Lonely, You can Always Come to me and Chat With Me!
+Salam, mən Ledy, Ağıllı ChatBotam. Əgər özünüzü tənha hiss edirsinizsə, hər zaman yanıma gəlib mənimlə söhbət edə bilərsiniz!
 """
 
 
 @LYCIA.on_message(filters.command(["start"], prefixes = "/") & ~filters.edited)
 async def info(client, message):
     buttons = [
-                [InlineKeyboardButton("Lycia", switch_inline_query_current_chat="lycia "), InlineKeyboardButton("Github", url = "https://github.com/Red-Aura/Lyciachatbot")]
+                [InlineKeyboardButton("Ledy", switch_inline_query_current_chat="lycia "), InlineKeyboardButton("Owner", url = "https://t.me/Tenha055")]
               ]
     await LYCIA.send_message(chat_id = message.chat.id, text = LYCIA_START, reply_markup = InlineKeyboardMarkup(buttons))
